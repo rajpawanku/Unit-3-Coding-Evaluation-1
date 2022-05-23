d1d9c6e130a5523c5019776d87a35b09ef7a35c8
Pawan Kumar <101585236+rajpawanku@users.noreply.github.com>
2022-05-23 16:58:07 +0530

    add

--- scripts/inventory.js
+++ scripts/inventory.js
@@ -9,17 +9,15 @@ function append(){
         let type = document.createElement('h1')
         type.innerText = el.type;
         type.style.textAlign="center"
-        type.setAttribute("class","type1")
 
         let Description = document.createElement('h3')
         Description.innerText = el.desc;
         Description.style.textAlign="center"
-        Description.setAttribute("class","text2")
-
+    
         let Price = document.createElement('h5')
         Price.innerText = el.price;
         Price.style.textAlign="center"
-        Price.setAttribute("class","text")
+        Price.style.fontSize="20px"
 
         let Image =document.createElement('img')
         Image.src= el.image;
@@ -30,16 +28,20 @@ function append(){
        // btn.style.marginLeft="37%"
         btn.setAttribute("id","Remove_button")
         btn.addEventListener("click",function(){
-             remove(index)
+             remove(elem,i)
         })
         div.append(Image,type,Description,Price,btn)
         container.append(div)
     })
 }
 append()
-function remove(index){
-    let products =JSON.parse(localStorage.getItem("products")) || [];
-    products.filter(function(el,i){
-        console.log(products)
-    })
+function remove(elem,i){
+    console.log(i)
+    products.splice(i,1)
+    localStorage.setItem("products",JSON.stringify(products))
+    // let products =JSON.parse(localStorage.getItem("products")) || [];
+    // products.filter(function(el,i){
+    //    // console.log(products)
+    // })
+   // localStorage.removeItem("products")
 }
